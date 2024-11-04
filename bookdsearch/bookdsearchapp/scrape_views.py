fbbd24ea32eda866a4182d994c92ab0770b19e46
Arnold <39159396+asiimwearnold@users.noreply.github.com>
2024-11-04 09:31:32 +0300

    Fix Error: Added Error loging

--- bookdsearch/bookdsearchapp/scrape_views.py
+++ bookdsearch/bookdsearchapp/scrape_views.py
@@ -1,11 +1,43 @@
+# from django.views.decorators.csrf import csrf_exempt
+# from .scraping_utils import scrape_book_data
+# import pandas as pd
+# import logging
+# from django.http import JsonResponse
+# import traceback
+
+# logger = logging.getLogger(__name__)
+# @csrf_exempt
+# def scrape_data(request):
+#     if request.method == 'POST':
+#         try:
+#             book_data = request.POST.getlist('book_data[]')
+#             scraped_data = []
+
+#             for book in book_data:
+#                 title, author = book.split('|')
+#                 genre, tags, image_url = scrape_book_data(title, author)
+#                 scraped_data.append({
+#                     'title': title,
+#                     'author': author,
+#                     'genre': genre,
+#                     'tags': ', '.join(tags),
+#                     'image_url': image_url
+#                 })
+
+#             return JsonResponse({'scraped_data': scraped_data})
+#         except Exception as e:
+#             logger.error(f"Error scraping data: {e}")
+#             logger.error(traceback.format_exc())
+#             return JsonResponse({'error': 'An error occurred while scraping the data.'}, status=500)
+#     return JsonResponse({'error': 'Invalid request method.'}, status=400)
 from django.views.decorators.csrf import csrf_exempt
 from .scraping_utils import scrape_book_data
-import pandas as pd
 import logging
 from django.http import JsonResponse
 import traceback
 
 logger = logging.getLogger(__name__)
+
 @csrf_exempt
 def scrape_data(request):
     if request.method == 'POST':
@@ -26,7 +58,8 @@ def scrape_data(request):
 
             return JsonResponse({'scraped_data': scraped_data})
         except Exception as e:
+            # Enhanced logging
             logger.error(f"Error scraping data: {e}")
             logger.error(traceback.format_exc())
-            return JsonResponse({'error': 'An error occurred while scraping the data.'}, status=500)
+            return JsonResponse({'error': f'An error occurred while scraping the data: {str(e)}'}, status=500)
     return JsonResponse({'error': 'Invalid request method.'}, status=400)
