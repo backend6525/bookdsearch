316da2a8e0f58c2ebe496179c6157492fcb1944c
Arnold <39159396+asiimwearnold@users.noreply.github.com>
2024-11-04 09:57:26 +0300

    Feat: Added Save data button

--- bookdsearch/bookdsearchapp/urls.py
+++ bookdsearch/bookdsearchapp/urls.py
@@ -3,9 +3,12 @@
 from django.urls import path
 from . import views
 from . import scrape_views  # Add this import
+from .scrape_views import scrape_data, save_data 
 
 urlpatterns = [
     path('', views.upload_file, name='upload_file'),
     path('upload/', views.upload_file, name='upload_file'),
-    path('scrape/', scrape_views.scrape_data, name='scrape_data'),  # Now this will work
+    path('scrape/', scrape_data, name='scrape_data'),
+    path('save/', save_data, name='save_data'), 
+      # Now this will work
 ]
